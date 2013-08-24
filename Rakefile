a3bdeacd3ffa7babd0ca80fc255cd3528a9c8adb
Jim Jones <jjones@aantix.com>
2013-08-24 01:50:30 -0700

    Adding a rake task to be called periodically to created Mechanical Turk tasks for image approval.

--- Rakefile
+++ Rakefile
@@ -5,3 +5,9 @@ require File.expand_path('../config/application', __FILE__)
 require 'tasks/turkee'
 
 ImageTurkModeration::Application.load_tasks
+
+
+desc "Posts to Mechanical Turk all images that haven't been approved or not."
+task :moderate_images => :environment do
+  Image.process_items
+end
--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,16 @@ class Image < ActiveRecord::Base
   def self.unprocessed
     where(processed: false)
   end
+
+  def self.process_items
+    unprocessed.each do |image|
+
+      Turkee::TurkeeTask.create_hit(host, "Is the given image pornographic or not?.",
+                                    hit_description, Image, num_assignments, reward,
+                                    lifetime, duration, {}, {:url_id => url.id}, {})
+
+      image.processed = true
+      image.save!
+    end
+  end
 end
